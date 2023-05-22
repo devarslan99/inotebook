b5d46e0f72ab51e538d5bfb51a3ec8f1f5e20cb1
devarslan99 <devarslan99@gmail.com>
2023-05-22 11:57:15 +0500

    creating Routes and Schema for Storing Notes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+////available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

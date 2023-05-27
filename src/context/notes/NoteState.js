ba6ffcd2c87a82dfb745a783d84e6afb7e8780d9
devarslan99 <devarslan99@gmail.com>
2023-05-27 11:50:00 +0500

    Adding font awesome icons to iNotebook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,7 +6,7 @@ const NoteState = (props) => {
 
      const notesinitial=[
         {
-          "_id": "646f0d700e793e3b4a26266a",
+          "_id": "646f0d700e793e3bc4a26266a",
           "user": "646df801f05ce815712dcb7f",
           "title": "political crises",
           "description": "ik arrested and banned to jail beajj ksdjhsue jjdsjdksdsdsdddd",
@@ -15,7 +15,7 @@ const NoteState = (props) => {
           "__v": 0
         },
         {
-            "_id": "646f0d700e793e3b4a26266a",
+            "_id": "646f0d700e793de3b4ca26266a",
             "user": "646df801f05ce815712dcb7f",
             "title": "political crises",
             "description": "ik arrested and banned to jail beajj ksdjhsue jjdsjdksdsdsdddd",
@@ -24,7 +24,7 @@ const NoteState = (props) => {
             "__v": 0
           },
           {
-            "_id": "646f0d700e793e3b4a26266a",
+            "_id": "646f0d700e7c93ed3b4a26266a",
             "user": "646df801f05ce815712dcb7f",
             "title": "political crises",
             "description": "ik arrested and banned to jail beajj ksdjhsue jjdsjdksdsdsdddd",
@@ -33,7 +33,7 @@ const NoteState = (props) => {
             "__v": 0
           },
           {
-            "_id": "646f0d700e793e3b4a26266a",
+            "_id": "646f0d700e793dce3b4a26266a",
             "user": "646df801f05ce815712dcb7f",
             "title": "political crises",
             "description": "ik arrested and banned to jail beajj ksdjhsue jjdsjdksdsdsdddd",
@@ -42,7 +42,7 @@ const NoteState = (props) => {
             "__v": 0
           },
           {
-            "_id": "646f0d700e793e3b4a26266a",
+            "_id": "646f0d700e793ced3b4a26266a",
             "user": "646df801f05ce815712dcb7f",
             "title": "political crises",
             "description": "ik arrested and banned to jail beajj ksdjhsue jjdsjdksdsdsdddd",
